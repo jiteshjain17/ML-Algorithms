db79b72e7aa45c4e0bc71b05d5d993872886a87b
jiteshjain17 <66688144+jiteshjain17@users.noreply.github.com>
2020-06-15 00:32:40 +0530

    Add files via upload

---
+++ week4 svr.py
@@ -0,0 +1,53 @@
+#Importing the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Importing the dataset
+dataset = pd.read_csv("C:/Users/jites/Desktop/datasets_88705_204267_Real estate.csv")
+X = dataset.iloc[:, :-1]
+y = dataset.iloc[:, -1]
+
+#Visualising the data
+plt.figure(figsize=(10,8))
+plt.subplot(221, title="X2 House age")
+sns.distplot(X['X2 house age'])
+
+plt.subplot(222, title="Distance Nearest MRT Station", facecolor = 'y')
+sns.distplot(X['X3 distance to the nearest MRT station'])
+
+plt.subplot(223, title="Convenience Stores", facecolor = 'w')
+sns.distplot(X['X4 number of convenience stores'])
+
+plt.subplot(224, title="Serial Number", facecolor = 'y')
+sns.distplot(X['No'])
+plt.show()
+
+#Splitting the dataset into training and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+#Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit_transform(X_test)
+
+#Performance Analysis
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+def reg_score(y_test, y_pred):
+    print("RMSE score: ", np.sqrt(mean_squared_error(y_test, y_pred)))
+    print("r2 score: ", r2_score(y_test, y_pred))
+    print("MAE score: ", mean_absolute_error(y_test, y_pred))
+
+#Fitting Random forest to dataset
+from sklearn.svm import  SVR
+svr = SVR(kernel='rbf')
+svr.fit(X_train, y_train)
+svr.score(X_test, y_test)
+
+#predicting test set results
+y_pred_svr = svr.predict(X_test)
+
+reg_score(y_test, y_pred_svr)
